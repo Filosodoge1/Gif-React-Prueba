9d57a680e08403ed713c6769e831f3f02c4e74fc
Edison Pineda Nacipucha <edison.pinedan@ug.edu.ec>
2022-06-19 14:39:56 -0500

    Agregando Nuevo componente GifGrid  a GifExpertApp

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,38 +1,30 @@
 import React, { useState } from "react";
 import { AddCategory } from "./Componentes/AddCategory";
+import { GifGrid } from "./Componentes/GifGrid";
 
 
 export const Funcion = ({value}) => {
-    
-    //let categories = ['dragon ball', 'naruto', 'one piece'];
 
     let [Categories, setCategories] = useState(['dragon ball', 'naruto', 'one piece']);
 
     const bingresar = (newCategory) => {
-        //console.log(value);
-        // setCategories([...Categories, value]);
-        // setCategories([Categories.push(value)]);
         if (Categories.includes(newCategory)) return;
         setCategories([...Categories, newCategory]);
         console.log('hola', newCategory);
     };
-    // console.log(Categories);
 
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr />
 
-            {/* <AddCategory setCategories={setCategories=Categories} /> */}
             <AddCategory onNewCategory={ (ca) => bingresar(ca)} />
 
-            <ol>
+            <>
                 {
-                    Categories.map(Categories => {
-                        return <li key={Categories}>{Categories}</li>
-                    })
+                    Categories.map(category => <GifGrid key={category} category={category} />)
                 }
-            </ol>
+            </>
         </>
     );
 };
