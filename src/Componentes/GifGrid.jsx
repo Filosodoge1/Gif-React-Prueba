292134f3664ce067e696a9dee9c4d79b068cdfcf
Edison Pineda Nacipucha <edison.pinedan@ug.edu.ec>
2022-07-19 18:55:17 -0500

    Casi obteniendo los id y tittle del GifApi

--- src/Componentes/GifGrid.jsx
+++ src/Componentes/GifGrid.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useState } from 'react';
 import GifApi from '../Helpers/GifApi'
 
-const GifGrid = ({key, category}) => {
-  GifApi(category);
+const GifGrid = ({category}) => {
+  const [first, setfirst] = useState([]);
+
+  const arregloImg = async() => {
+    const newImg = await GifApi(category);
+    setfirst(newImg);
+  }
+
+  
+
+  //GifApi(category);
   return (
     <div>
         <h1>{category}</h1>
+        <ol>
+          {
+            first.map(
+              ({id, title}) => (
+                <li key={id}>{title}</li>
+              )
+            )
+          }
+        </ol>
     </div>
   )
 }
