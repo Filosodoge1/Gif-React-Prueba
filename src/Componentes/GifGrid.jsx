d6211b9bd972f321e8e16b9385b2f1413518d1fb
Edison Pineda Nacipucha <edison.pinedan@ug.edu.ec>
2022-07-19 17:57:49 -0500

    Agregando GifApi

--- src/Componentes/GifGrid.jsx
+++ src/Componentes/GifGrid.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import GifApi from '../Helpers/GifApi'
 
 const GifGrid = ({key, category}) => {
-  
+  GifApi(category);
   return (
     <div>
         <h1>{category}</h1>
---
+++ src/Helpers/GifApi.js
@@ -0,0 +1,13 @@
+const GifApi = async(variable) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=Ora2a1BRyp5v2EqVo4qAEkqeR2GJ4p6T&q=${variable}&limit=10`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.downsized.medium
+    }));
+  return gifs;
+}
+
+export default GifApi
